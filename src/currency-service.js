718085fe3b7a941fb1c2920c8ab42bf5acd57ab9
joeroaden <106051349+joeroaden@users.noreply.github.com>
2022-07-01 10:07:11 -0700

    WIP, build out index.js functions and begin doc ready function

--- src/currency-service.js
+++ src/currency-service.js
@@ -1,6 +1,6 @@
 export default class CurrencyService {
-  static getExchange() {
-    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/USD`)
+  static getExchange(amount) {
+    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/USD/${amount}`)
       .then(function (response) {
         if (!response.ok) {
           throw Error(response.statusText);
--- src/index.js
+++ src/index.js
@@ -1,8 +1,29 @@
+import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-// import $ from 'jquery';
+import CurrencyService from './currency-service';
 
+function clearFields () {
+  $('#amount').val("");
+}
 
+function displayCurrencies (arabEmirates, afghan, albanian, armenian, netherlands) {
+  $(".showArabEmirates").text(`Arab Emirates (AED): ${arabEmirates} `);
+  $(".showAfghan").text(`Afghan (AFN): ${afghan}`);
+  $(".showAlbanian").text(`Albanian (ALL): ${albanian}`);
+  $(".showArmenian").text(`Armenian (AMD): ${armenian}`);
+  $(".showNetherlands").text(`Netherlands (ANG): ${netherlands}`);
+}
 
-// import { Object } from './businesslog.js';
+function displayErrors(error) {
+  $('.showErrors').text(`${error}`);
+}
+
+$(document).ready(function () {
+  $("#getExchange").click(function () {
+    let amount = $("#amount").val();
+    clearFields();
+    
+  }
+}
