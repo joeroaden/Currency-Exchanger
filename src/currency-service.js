640c1b7a1572cba660af907299580b8835d34193
joeroaden <106051349+joeroaden@users.noreply.github.com>
2022-07-01 09:12:52 -0700

    create currency service js class and getExchange method

--- src/currency-service.js
+++ src/currency-service.js
@@ -1,4 +1,14 @@
-// export class Object {
-//   constructor() {
-//   }
-// }
+export default class CurrencyService {
+  static getExchange() {
+    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/USD`)
+      .then(function (response) {
+        if (!response.ok) {
+          throw Error(response.statusText);
+        }
+        return response.json();
+      })
+      .catch(function (error) {
+        return error;
+      });
+  }
+}
